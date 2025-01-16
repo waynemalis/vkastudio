39941e03885eb7e90280990f4f9b4b9fbb75e18e
waynemalis <80430848+waynemalis@users.noreply.github.com>
2025-01-16 16:08:16 -0600

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -1,12 +1,9 @@
-window.addEventListener('scroll', function() {
-  const navbar = document.getElementById('nav');
-  const navLinks = document.getElementById("navLinks");
-  // Verificar si el scroll ha bajado más de 100 píxeles
-  if (window.scrollY > 500) {
+const navbar = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 120) {
     navbar.classList.add('scrolled');
   } else {
-    if (navLinks.style.display != "block") {
-      navbar.classList.remove('scrolled');
-    }
+    navbar.classList.remove('scrolled');
   }
 });
